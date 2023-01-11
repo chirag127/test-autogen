2766b304fda258579f220fd1b8616daa8a97de96
Chirag <76880977+chirag127@users.noreply.github.com>
2023-01-11 19:26:12 +0530

    trelent

--- codwisper.py
+++ codwisper.py
@@ -2,13 +2,8 @@ from typing import List
 
 
 def has_close_elements(numbers: List[float], threshold: float) -> bool:
-    """ Check if in given list of numbers, are any two numbers closer to each other than
-    given threshold.
-    >>> has_close_elements([1.0, 2.0, 3.0], 0.5)
-    False
-    >>> has_close_elements([1.0, 2.8, 3.0, 4.0, 5.0, 2.0], 0.3)
-    True
-    """
+
+
 
 
 
@@ -19,13 +14,16 @@ def has_close_elements(numbers: List[float], threshold: float) -> bool:
 
 
 def separate_paren_groups(paren_string: str) -> List[str]:
-    """ Input to this function is a string containing multiple groups of nested parentheses. Your goal is to
-    separate those group into separate strings and return the list of those.
-    Separate groups are balanced (each open brace is properly closed) and not nested within each other
-    Ignore any spaces in the input string.
-    >>> separate_paren_groups('( ) (( )) (( )( ))')
-    ['()', '(())', '(()())']
     """
+    The separate_paren_groups function takes a string of parentheses, and separates it into groups.
+    For example: separate_paren_groups('(())') -&gt; ['(', ')']
+
+    :param paren_string: str: Specify the string that will be passed to the function
+    :return: A list of strings
+    :doc-author: Trelent
+    """
+    
+    return 1
 
 
 
@@ -2858,7 +2856,9 @@ def do_algebra(operator, operand):
         Operator list has at least one operator, and operand list has at least two operands.
 
     """
-    
+
+
+
 
 
 
